4d50e86d99835369a3fc8adfae30f21d9133d736
Paweł Bielicki <bielicki91@gmail.com>
2014-05-04 18:49:14 +0200

    add Fluent constructors

--- KnowledgeRepresentationIntelligence/World/Fluent.cs
+++ KnowledgeRepresentationIntelligence/World/Fluent.cs
@@ -7,6 +7,17 @@
         public bool Value { get; set; }
         public int? ReleaseAt { get; set; }
 
+        public Fluent(string name, bool p)
+        {
+            this.Name = name;
+            Value = p;
+        }
+
+        public Fluent()
+        {
+            // TODO: Complete member initialization
+        }
+
         public override string ToString()
         {
             string description = "Fluent: " + Id + ": name: " + Name + " , value: " + Value;
