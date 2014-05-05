88c933ecbe30adb8c1cd7fbe39ec972e6b9523c6
cyryl2004 <cyryl2004@gmail.com>
2014-05-05 23:35:32 +0200

    Scenario draft

---
+++ KnowledgeRepresentationInterface/Views/ScenarioControls/ScenarioAddActionControl/SceAddAction.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KnowledgeRepresentationInterface.Views.ScenarioControls.ScenarioAddActionControl
+{
+    /// <summary>
+    /// Interaction logic for SceAddAction.xaml
+    /// </summary>
+    public partial class SceAddAction : UserControl, ISceControl
+    {
+        public SceAddAction()
+        {
+            InitializeComponent();
+        }
+
+        public void CleanValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
