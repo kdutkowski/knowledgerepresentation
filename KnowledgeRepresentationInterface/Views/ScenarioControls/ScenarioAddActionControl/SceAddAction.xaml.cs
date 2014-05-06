5b4ebea593e9c4d59ad2c345e377ff2d08e6ebf9
cyryl2004 <cyryl2004@gmail.com>
2014-05-06 10:25:26 +0200

    Time binding

--- KnowledgeRepresentationInterface/Views/ScenarioControls/ScenarioAddActionControl/SceAddAction.xaml.cs
+++ KnowledgeRepresentationInterface/Views/ScenarioControls/ScenarioAddActionControl/SceAddAction.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,30 @@ namespace KnowledgeRepresentationInterface.Views.ScenarioControls.ScenarioAddAct
     /// <summary>
     /// Interaction logic for SceAddAction.xaml
     /// </summary>
-    public partial class SceAddAction : UserControl, ISceControl
+    public partial class SceAddAction : UserControl, ISceControl, INotifyPropertyChanged
     {
+        //TODO implement validation Time
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private int _time;
+
+        public int Time
+        {
+            get { return _time; }
+            set { _time = value;
+             OnPropertyChanged("Time");}
+        }
+        
+        
+        
         public SceAddAction()
         {
             InitializeComponent();
@@ -32,7 +55,7 @@ namespace KnowledgeRepresentationInterface.Views.ScenarioControls.ScenarioAddAct
 
         private void ButtonAddAction_Click(object sender, RoutedEventArgs e)
         {
-
+            LabelValidation.Content = _time.ToString();
         }
     }
 }
