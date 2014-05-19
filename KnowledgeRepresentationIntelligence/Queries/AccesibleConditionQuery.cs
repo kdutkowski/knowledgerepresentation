90e54e42d38a4bdcaa47100c94e3950cb24260d9
Mariusz <armaniel667@gmail.com>
2014-05-19 21:47:07 +0200

    AccesibleConditionQuery and ExecutableScenarioQuery implemented.

--- KnowledgeRepresentationIntelligence/Queries/AccesibleConditionQuery.cs
+++ KnowledgeRepresentationIntelligence/Queries/AccesibleConditionQuery.cs
@@ -3,20 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KnowledgeRepresentationReasoning.Expressions;
+using KnowledgeRepresentationReasoning.Scenario;
+
 
 namespace KnowledgeRepresentationReasoning.Queries
 {
     public class AccesibleConditionQuery : Query
     {
-        public AccesibleConditionQuery(QuestionType questionType)
+        private string _condition;      //condition to check 
+        private SimpleLogicExpression _logicExp;
+        private string[] _fluentNames;
+        ScenarioDescription _scenario;
+
+
+        public AccesibleConditionQuery(QuestionType questionType, string condition, ScenarioDescription scenario)
             : base(QueryType.AccesibleCondition, questionType)
         {
             _queryType = QueryType.AccesibleCondition;
+            _condition = condition;
+            _logicExp = new SimpleLogicExpression(_condition);
+            _fluentNames = _logicExp.GetFluentNames();
+            _scenario = scenario;
+            _logger.Info("Creates:\n " + this.ToString());
+
         }
 
+
+        /// <summary>
+        /// Checks given parameters with conditionAtTimeQuery and ExecutableScenarioQuery
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="worldAction"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
         public override QueryResult CheckCondition(World.State state, World.WorldAction worldAction, int time)
         {
-            throw new NotImplementedException();
+            _logger.Info("Checking condition: " + _condition + "\n accesible with parameters:\nstate: " + state.ToString() + "\naction: " + worldAction ?? worldAction.ToString());
+            QueryResult result = QueryResult.Undefined;
+
+            Query query = new ConditionAtTimeQuery(QuestionType.Ever, _condition, time);
+
+            result = query.CheckCondition(state, worldAction, time);
+            if (result == QueryResult.Undefined || result == QueryResult.Error || result == QueryResult.False)
+            {
+
+                return result;
+            }
+
+            query = new ExecutableScenarioQuery(QuestionType.Ever, _scenario);
+            result = query.CheckCondition(state, worldAction, time);
+            if (result == QueryResult.Undefined || result == QueryResult.Error || result == QueryResult.False) return result;
+
+            string logResult = "Accesible: " + result;
+
+            if (QueryResult.Undefined == result)
+                _logger.Warn(logResult);
+            else
+                _logger.Info(logResult);
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder("Accesible Condition Query:\ncondition: ", 77);
+            stringBuilder.Append(_condition);
+            stringBuilder.Append("\nscenario:\n");
+            stringBuilder.Append(_scenario);
+            return stringBuilder.ToString();
         }
     }
+
+
+
 }
--- KnowledgeRepresentationIntelligence/Queries/ExecutableScenarioQuery.cs
+++ KnowledgeRepresentationIntelligence/Queries/ExecutableScenarioQuery.cs
@@ -1,15 +1,61 @@
-﻿namespace KnowledgeRepresentationReasoning.Queries
-{
-    using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KnowledgeRepresentationReasoning.Scenario;
 
+namespace KnowledgeRepresentationReasoning.Queries
+{
     class ExecutableScenarioQuery : Query
     {
-        public ExecutableScenarioQuery(QuestionType questionType)
-            : base(QueryType.ExecutableScenario, questionType) { }
 
+        ScenarioDescription _scenario;
+        public ExecutableScenarioQuery(QuestionType questionType, ScenarioDescription scenario)
+            : base(QueryType.ExecutableScenario, questionType)
+        {
+            _scenario = scenario;
+        }
+        /// <summary>
+        /// Checks whetever actions and observations in this query are consistent with those 
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="worldAction"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
         public override QueryResult CheckCondition(World.State state, World.WorldAction worldAction, int time)
         {
-            throw new NotImplementedException();
+
+            _logger.Info("Checking if scenario: " + _scenario.ToString() + " with parameters:\nstate: " + state.ToString() + "\naction: " + worldAction ?? worldAction.ToString() + " is executable");
+
+            QueryResult result = QueryResult.Undefined;
+            foreach (ScenarioObservationRecord sor in _scenario.observations)
+                if (sor.Time == time)
+                    if (!sor.Expr.Evaluate(state)) result = QueryResult.False;
+
+            if (result == QueryResult.True)
+                foreach (ScenarioActionRecord sar in _scenario.actions)
+                    if (sar.Time == time)
+                        if (sar.CheckIfActiveAt(time) && !sar.WorldAction.Equals(worldAction)) result = QueryResult.False;
+
+            string logResult = "Executable: " + result;
+
+            if (QueryResult.Undefined == result)
+                _logger.Warn(logResult);
+            else
+                _logger.Info(logResult);
+
+            return result;
+            //            throw new NotImplementedException();
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder("Executable Scenario Query:\nscenario: ", 77);
+            stringBuilder.Append(_scenario);
+            return stringBuilder.ToString();
         }
     }
-}
+ }
+
