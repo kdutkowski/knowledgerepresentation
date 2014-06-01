4ab40c4761f0460a731ba5b170be9aeefa828a8a
cyryl2004 <cyryl2004@gmail.com>
2014-06-01 19:01:51 +0200

    Prepare to tests from documentation examples

---
+++ KnowledgeRepresentationReasoning.Test/DocumentationExamples/Example2.cs
@@ -0,0 +1,62 @@
+﻿using KnowledgeRepresentationReasoning.World;
+using NUnit.Framework;
+
+namespace KnowledgeRepresentationReasoning.Test.DocumentationExamples
+{
+    [TestFixture]
+    public class Example2 : TestBase
+    {
+        #region | ACTIONS |
+        private readonly WorldAction _makingPancAction = new WorldAction
+        {
+            Id = "makingPanc",
+            Duration = 1
+        };
+        private readonly WorldAction _makingCakeAction = new WorldAction
+        {
+            Id = "makingCake",
+            Duration = 1
+        };
+        private readonly WorldAction _buyEggsAction = new WorldAction
+        {
+            Id = "buyEggs",
+            Duration = 2
+        };
+        #endregion
+
+        #region | FLUENTS |
+        private Fluent _eggsFluent = new Fluent()
+        {
+            Name = "eggs"
+        };
+        #endregion
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        #region | EXAMPLE 2 |
+
+        //Mick i Sarah sa para, wiec maja wspólne produkty spozywcze, ale posiłki zwykle jadaja oddzielnie.
+        //Pewnego dnia Sarah chce zrobic ciasto, a Mick nalesniki. Nie moga byc one robione w tym samym czasie
+        //ze wzgledu koniecznosc uzycia miksera do przygotowania obu. Ponadto, zrobienie jednego lub drugiego
+        //dania zuzywa cały zapas jajek dostepnych w mieszkaniu, wiec trzeba je potem dokupic.
+
+        //4.2.2 Opis akcji
+        //(making panc; 1) causes !eggs if eggs
+        //(making cake; 1) causes !eggs if eggs
+        //(buy eggs; 2) causes eggs
+
+        //4.2.3 Scenariusz
+        //Sc =(OBS; ACS)
+        //OBS = f(eggs; 0)g
+        //ACS = f((making panc; 1); 0); ((making cake; 1); 2)g
+
+        //4.2.4 Kwerendy
+        //1. eggs at 0 when Sc
+        //2. eggs at 2 when Sc
+
+
+        #endregion
+    }
+}
---
+++ KnowledgeRepresentationReasoning.Test/DocumentationExamples/Example4.cs
@@ -0,0 +1,67 @@
+﻿using KnowledgeRepresentationReasoning.World;
+using NUnit.Framework;
+
+namespace KnowledgeRepresentationReasoning.Test.DocumentationExamples
+{
+    [TestFixture]
+    public class Example4 : TestBase
+    {
+        #region | ACTIONS |
+        private readonly WorldAction _clicksButtonAction = new WorldAction
+        {
+            Id = "clicksButton",
+            Duration = 1
+        };
+        private readonly WorldAction _switchesOnComputerAction = new WorldAction
+        {
+            Id = "switchesOnComputer",
+            Duration = 2
+        };
+        private readonly WorldAction _disconnectsPowerAction = new WorldAction
+        {
+            Id = "disconnectsPower",
+            Duration = 1
+        };
+        #endregion
+
+        #region | FLUENTS |
+        private Fluent _connectPowerComputerFluent = new Fluent()
+        {
+            Name = "connectPowerComputer"
+        };
+        private Fluent _onComputerFluent = new Fluent()
+        {
+            Name = "onComputer"
+        };
+        #endregion
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        #region | EXAMPLE 4 |
+        //Mamy Billa oraz komputer. Bill moze nacisnac przycisk Włacz lub odłaczyc komputer od zasilania.
+        //Poczatkowo komputer jest wyłaczony i podłaczony do zasilania. Jezeli zostanie nacisniety jego przycisk
+        //Włacz oraz komputer jest podłaczony do zasilania, to komputer właczy sie. Odłaczenie komputera od
+        //pradu powoduje, ze komputer bedzie odłaczony od zasilania oraz wyłaczony.
+
+        //4.4.2 Opis akcji
+        //(clicks button on; 1) invokes (switches on computer; 2) after 0 if connect power computer
+        //(switches on computer; 2) causes on computer
+        //(disconnects power; 1) causes !connect power computer
+        //(disconnects power; 1) causes !on computer
+
+        //4.4.3 Scenariusz
+        //Sc =(OBS; ACS)
+        //OBS = f(!on computer; 0); (connect power computer; 0)g
+        //ACS = f(clicks button on; 1); 1); ((disconnects power; 1); 4); ((clicks button on; 1); 5)g
+
+        //4.4.4 Kwerendy
+        //1. on computer at 7 when Sc
+        //2. accesible on computer when Sc
+
+
+        #endregion
+    }
+}
