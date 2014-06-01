9b77a3d36cbd702305a9252a1de32328ab0bb0cd
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-06-01 18:41:46 +0200

    nothing works - I am angry!!

--- KnowledgeRepresentationReasoning.Test/ReasoningTests.cs
+++ KnowledgeRepresentationReasoning.Test/ReasoningTests.cs
@@ -1,8 +1,5 @@
 ﻿namespace KnowledgeRepresentationReasoning.Test
 {
-    using System;
-    using System.Linq;
-
     using KnowledgeRepresentationReasoning.Expressions;
     using KnowledgeRepresentationReasoning.Queries;
     using KnowledgeRepresentationReasoning.Scenario;
@@ -10,7 +7,6 @@
     using KnowledgeRepresentationReasoning.World.Records;
 
     using NUnit.Framework;
-    using NUnit.Framework.Constraints;
 
     [TestFixture]
     public class ReasoningTests : TestBase
@@ -228,16 +224,18 @@
 
         public void SetComplexWorldDescription_A()
         {
-            _reasoning.AddWorldDescriptionRecord(new ActionCausesIfRecord(this._actionA2, "!c", "c"));
-            _reasoning.AddWorldDescriptionRecord(new ActionCausesIfRecord(this._actionB3, "a || b", "d"));
-            _reasoning.AddWorldDescriptionRecord(new ActionInvokesAfterIfRecord(this._actionB3, this._actionC3, 5, "a"));
-            _reasoning.AddWorldDescriptionRecord(new ActionInvokesAfterIfRecord(this._actionB3, this._actionD1, 1, "c"));
-            _reasoning.AddWorldDescriptionRecord(new ActionReleasesIfRecord(this._actionD1, new Fluent("d", true), ""));
-            _reasoning.AddWorldDescriptionRecord(new ExpressionTriggersActionRecord(this._actionC5, "a && !c && d"));
-            _reasoning.AddWorldDescriptionRecord(new ImpossibleActionAtRecord(this._actionC5, 50));
-            _reasoning.AddWorldDescriptionRecord(new ImpossibleActionIfRecord(this._actionA2, "!a && !b"));
-            _reasoning.AddWorldDescriptionRecord(new ActionInvokesAfterIfRecord(this._actionA2, this._actionC5, 12, "d"));
-            _reasoning.AddWorldDescriptionRecord(new ActionReleasesIfRecord(this._actionC5, new Fluent("c", true), "d || b"));
+            _reasoning = new Reasoning();
+            _reasoning.AddWorldDescriptionRecord(new InitialRecord("a || b || c || d"));                                        // (0)
+            _reasoning.AddWorldDescriptionRecord(new ActionCausesIfRecord(this._actionA2, "!c", "c"));                          // (1)
+            _reasoning.AddWorldDescriptionRecord(new ActionCausesIfRecord(this._actionB3, "a || b", "d"));                      // (2)
+            _reasoning.AddWorldDescriptionRecord(new ActionInvokesAfterIfRecord(this._actionB3, this._actionC3, 5, "a"));       // (3)
+            _reasoning.AddWorldDescriptionRecord(new ActionInvokesAfterIfRecord(this._actionB3, this._actionD1, 1, "c"));       // (4)
+            _reasoning.AddWorldDescriptionRecord(new ActionReleasesIfRecord(this._actionD1, new Fluent("d", true), ""));        // (5)
+            _reasoning.AddWorldDescriptionRecord(new ExpressionTriggersActionRecord(this._actionC5, "a && !c && d"));           // (6)
+            _reasoning.AddWorldDescriptionRecord(new ImpossibleActionAtRecord(this._actionC5, 50));                             // (7)
+            _reasoning.AddWorldDescriptionRecord(new ImpossibleActionIfRecord(this._actionA2, "!a && !b"));                     // (8)
+            _reasoning.AddWorldDescriptionRecord(new ActionInvokesAfterIfRecord(this._actionA2, this._actionC5, 12, "d"));      // (9)
+            _reasoning.AddWorldDescriptionRecord(new ActionReleasesIfRecord(this._actionC5, new Fluent("c", true), "d || b"));  // (10)
         }
 
         public void SetComplexScenario_A()
@@ -246,7 +244,31 @@
             _scenarioDescription.addObservation(new SimpleLogicExpression("a && b && c && d"), 1);
             _scenarioDescription.addACS(this._actionB3, 3);
             _scenarioDescription.addACS(this._actionA2, 20);
-            _scenarioDescription.addObservation(new SimpleLogicExpression("a && !b && !c && d"), 24);
+            _scenarioDescription.addObservation(new SimpleLogicExpression("a && !b && !c && d"), 24);   // (1,0,0,1)
+
+            /* Wynik dla ComplexWorldDescription_A
+             * 
+             * Obserwacja czas 1: a = 1, b = 1, c = 1, d = 1 czyli (1,1,1,1)
+             * Akcja czas 3: (B,3) 
+             *      Rezultaty:
+             *              0) Czas 6: Możliwe stany: (0,1,1,1), (1,1,1,1), (1,0,1,1)
+             *              1) Czas 7: Rozpoczyna się akcja (D,1)
+             *              2) Czas 8: Zwalniany jest fluent d - 
+             *                  Możliwe stany: 
+             *                      (0,1,1,1), (0,1,1,0),
+             *                      (1,1,1,1), (1,1,1,0),
+             *                      (1,0,1,1), (1,0,1,0)
+             *              3) Czas 11: Rozpoczyna się akcja (C,3)
+             * Akcja czas 20: (A,2)
+             *      Rezultaty:
+             *              1) Czas 22: Możliwe stany:
+             *                      (0,1,0,1), (0,1,0,0),
+             *                      (1,1,0,1), (1,1,0,0),
+             *                      (1,0,0,1), (1,0,0,0)
+             *              2) Czas 22: Rozpoczyna się akcja (C,5)
+             *              3) Czas 34: Rozpoczyna się akcja (C,5)
+             *              
+             */
         }
 
         #region ExecutableScenarioQuery
@@ -325,6 +347,20 @@
 
             // Test
             var result = _reasoning.ExecuteQuery(new AccesibleConditionQuery(QuestionType.Always, "c", _scenarioDescription), _scenarioDescription);
+            Assert.AreEqual(QueryResult.True, result);
+        }
+
+        #endregion
+
+        #region PerformingActionAtTimeQuery
+
+        [Test]
+        public void Reasoning_PerformingActionAtTimeQuery_Ever_Complex_ActionInProgress_Test()
+        {
+            var result = _reasoning.ExecuteQuery(new PerformingActionAtTimeQuery(QuestionType.Always, this._actionC5, 24), _scenarioDescription);
+            Assert.AreEqual(QueryResult.True, result);
+
+            result = _reasoning.ExecuteQuery(new PerformingActionAtTimeQuery(QuestionType.Always, this._actionC5, 34), _scenarioDescription);
             Assert.AreEqual(QueryResult.True, result);
         }
 
